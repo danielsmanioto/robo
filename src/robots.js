d36dd97ac2acb3a59645bdadd5f28bba2ad4c236
Daniel Smanioto <daniel.smanioto@gmail.com>
2020-07-11 18:28:22 -0300

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,42 @@
+export const robots = [
+    {
+        id: "1",
+        name: "Daniel",
+        email: "daniel.smanioto@gmail.com"
+    },
+    {
+        id: "aslkdjkadkjaskldjklasdasdasd",
+        name: "Caroline",
+        email: "carol.smanioto@gmail.com"
+    },
+    {
+        id: "3",
+        name: "Manuela",
+        email: "manuela.de.costa@gmail.com"
+    },
+    {
+        id: "4",
+        name: "Perry",
+        email: "perry.smanioto@gmail.com"
+    },   
+    {
+        id: "5",
+        name: "Daniela",
+        email: "daniela.smanioto@gmail.com"
+    },
+    {
+        id: "6",
+        name: "Durvalino",
+        email: "dino.smanioto@gmail.com"
+    },
+    {
+        id: "7",
+        name: "Eutalia",
+        email: "eutalia.smanioto@gmail.com"
+    } ,
+    {
+        id: "8",
+        name: "Jozy",
+        email: "default@gmail.com"
+    }           
+]
